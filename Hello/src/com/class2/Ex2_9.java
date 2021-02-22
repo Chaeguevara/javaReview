ad36527498041378717adacd642860795c8337e9
Chaeguevara <dfso2@naver.com>
2021-02-22 23:20:54 +0900

    Chp2

---
+++ Hello/src/com/class2/Ex2_9.java
@@ -0,0 +1,19 @@
+package com.class2;
+
+public class Ex2_9 {
+
+	public static void main(String[] args) {
+		// Swap value
+		int x = 10, y = 5;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		
+		int tmp = x;
+		x = y;
+		y = tmp;
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		
+	}
+
+}
