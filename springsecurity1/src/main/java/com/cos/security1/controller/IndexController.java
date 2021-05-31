6b14936b251e18482312981740b1171a55c3a94a
Chaeguevara <dfso2@naver.com>
2021-05-31 16:31:27 +0900

    Secured어노테이션 및 PreAuthorize
     - SecurityConf에서 Enable시킴
     - 각 컨트롤러에 적용시킴
     - 용도에 따라서 하면됨

--- springsecurity1/src/main/java/com/cos/security1/controller/IndexController.java
+++ springsecurity1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.cos.security1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,53 +14,68 @@ import com.cos.security1.repository.UserRepository;
 
 @Controller
 public class IndexController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder; 
-	
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	@GetMapping({ "", "/" })
 	public String index() {
-		//머스ㅌ터치 사용함. 기본위치는 src/main/resources/
-		//뷰리졸버 : template(prefix), .mustache(suffix) -> application.yml
+		// 머스ㅌ터치 사용함. 기본위치는 src/main/resources/
+		// 뷰리졸버 : template(prefix), .mustache(suffix) -> application.yml
 		// Dependency로 생략 가능
-		return "index"; //src/main/resources/templates/index.mustach를 의미 -> config에서 처리
+		return "index"; // src/main/resources/templates/index.mustach를 의미 -> config에서 처리
 	}
+
 	@GetMapping("/user")
 	public @ResponseBody String user() {
 		return "user";
 	}
-	
+
 	@GetMapping("/admin")
 	public @ResponseBody String admin() {
 		return "admin";
 	}
+
 	@GetMapping("/manager")
 	public @ResponseBody String manager() {
 		return "manager";
 	}
+
 	@GetMapping("/loginForm")
 	public String loginForm() {
 		return "loginForm";
 	}
-	
+
 	@GetMapping("/joinForm")
 	public String joinForm() {
 		return "joinForm";
 	}
-	
+
 	@PostMapping("/join")
 	public @ResponseBody String join(User user) {
 		System.out.println(user);
 		user.setRole("ROLE_USER");
-		//ID와 CreationTIme은 어노테이션이 알아서 해줌
+		// ID와 CreationTIme은 어노테이션이 알아서 해줌
 		String rawPassword = user.getPassword();
 		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
 		user.setPassword(encPassword);
 		userRepository.save(user);// 회원가입 됨 but 비번 1234 -> 시큐리티 로그인 불가. 암호화 안됨.
 		return "redirect:/loginForm";
+	}
+
+	@Secured("ROLE_ADMIN") // 어드민만 볼 수 있다.
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
+
+	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") // 실행직전에 된다
+	@GetMapping("/data")
+	public @ResponseBody String data() {
+		return "데이터정보";
 	}
 
 }
