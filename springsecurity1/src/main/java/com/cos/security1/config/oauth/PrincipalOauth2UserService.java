5a76542d3ab425841f6166867a10e1b1eacc1722
Chaeguevara <dfso2@naver.com>
2021-06-03 12:01:53 +0900

    네이버 로그인 완료 -> JWT기본 이해

--- springsecurity1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ springsecurity1/src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config.oauth;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.cos.security1.config.auth.PrincipalDetails;
 import com.cos.security1.config.oauth.provider.FacebookUserInfo;
 import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.NaverUserInfo;
 import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
@@ -45,8 +48,12 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
 			System.out.println("페이스북 로그인 요청");
 			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
-		}else {
-			System.out.println("우리는 구글과 페이스북만...");
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+			System.out.println("네이버 로그인 요청");
+			oAuth2UserInfo = new NaverUserInfo((Map)oauth2User.getAttributes().get("response"));
+		}
+		else {
+			System.out.println("우리는 구글 / 페이스북 / 네이버만 지원합니다");
 		}
 		
 		String provider = oAuth2UserInfo.getProvider(); // Google or facebook
@@ -60,7 +67,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		User userEntity = userRepository.findByUsername(username);
 
 		if (userEntity == null) {
-			System.out.println("구글로그인이 최초입니다");
+			System.out.println("Oauth로그인이 최초입니다");
 			//Constructor와 같음. 어노테이션 활용
 			userEntity = User.builder()
 					.username(username)
@@ -72,7 +79,7 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 					.build();
 			userRepository.save(userEntity);
 		}else {
-			System.out.println("이미 구글로그인을 한적이 있습니다."); 
+			System.out.println("이미 로그인을 한적이 있습니다. 자동회원가입 되어있습니다"); 
 		}
 		// authentication객체에 들어갈 것임
 		return new PrincipalDetails(userEntity, oauth2User.getAttributes());
