8492cce2f376fb47a9efd34b2e2bd410f8a7995c
Chaeguevara <dfso2@naver.com>
2021-11-30 02:25:58 +0900

    TwoSum(Easy)

---
+++ leetcode/src/main/java/TwoSum.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+
+    public static void dupTest() {
+        Map<Integer,Integer> test = new HashMap<>();
+        test.put(1,1);
+        System.out.println(test.get(1));
+        test.put(1,2);
+        System.out.println(test.get(1));
+        test.put(1,3);
+        System.out.println(test.get(1));
+        test.get(1);
+
+    }
+
+    public static int[] addTwo(int[] nums, int target) {
+        int[] result = {0,0};
+        Map <Integer,int[]> numIndex = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int[] values = {-1,-1};
+            if(numIndex.containsKey(nums[i])){
+                values = numIndex.get(nums[i]);
+                values[1] = i;
+                numIndex.put(nums[i],values);
+                continue;
+            }
+            values[0] = i;
+            numIndex.put(nums[i],values);
+        }
+        for (Integer value : numIndex.keySet()){
+            result[0] = numIndex.get(value)[0];
+            if(numIndex.containsKey(target-value)) {
+                result[1] = numIndex.get(target-value)[0];
+                if (result[0]==result[1]){
+                    result[1] = numIndex.get(target-value)[1];
+                }
+                break;
+
+            }
+
+        }
+
+
+        return result;
+    }
+}
---
+++ leetcode/src/test/java/TwoSumTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumTest {
+
+    @Test
+    public void testAdd(){
+        Assertions.assertEquals(2,1+1);
+        Assertions.assertArrayEquals(new int[]{0, 1},TwoSum.addTwo(new int[]{2, 7, 11, 15},9));
+        Assertions.assertArrayEquals(new int[]{0, 1},TwoSum.addTwo(new int[]{3, 3},6));
+        Assertions.assertArrayEquals(new int[]{0, 3},TwoSum.addTwo(new int[]{3, 12,5,8},11));
+
+        TwoSum.dupTest();
+    }
+}
