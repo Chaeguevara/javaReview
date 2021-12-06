aa0b88d0f8f7e47e00dace30e444715bde00d333
Chaeguevara <dfso2@naver.com>
2021-12-06 17:02:20 +0900

    Security #2 Spring Web Security 설정

--- loginserver/src/main/java/com/loginServer/loginserver/controller/IndexController.java
+++ loginserver/src/main/java/com/loginServer/loginserver/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.loginServer.loginserver.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -12,4 +13,35 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "유저페이지";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "어드민 페이지";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/join")
+    public String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨!";
+    }
+
 }
