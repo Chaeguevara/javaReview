aac6caaa175db751817df86bdd89afee2625fe29
Chaeguevara <dfso2@naver.com>
2021-12-06 12:42:56 +0900

    Merge two sorted list on going

---
+++ leetcode/src/main/java/mergeTwoLists.java
@@ -0,0 +1,38 @@
+
+
+public class mergeTwoLists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        //base case
+        if (list1 == null || list2 == null){
+            return  null;
+        }
+
+        if (list2 == null){
+            return  list1;
+        }
+
+        if (list1 == null){
+            return  list2;
+        }
+
+        ListNode sentinel = new ListNode(-101,null);
+        ListNode curr=sentinel,curr1=list1,curr2=list2,succ=curr.next,succ1=curr1.next,succ2=curr2.next;
+        if (curr1.val <= curr2.val){
+            curr.next = curr1;
+        }else{
+            curr.next = mergeTwoLists(curr1,succ2);
+
+        }
+
+        return sentinel.next;
+
+    }
+}
---
+++ leetcode/src/main/java/romanToInt.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class romanToInt {
+    public static int solution(String s){
+        int result = 0;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("I",1);
+        map.put("V",5);
+        map.put("X",10);
+        map.put("L",50);
+        map.put("C",100);
+        map.put("D",500);
+        map.put("M",1000);
+        for (int i=0; i < s.length(); i++){
+
+        }
+
+        return result;
+    }
+}
