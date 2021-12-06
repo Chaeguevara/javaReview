e4c938fddcf5c25232b5b4d637295b7597662f82
Chaeguevara <dfso2@naver.com>
2021-12-06 16:20:30 +0900

    시큐리티 특강 1강
     서버설정 / Mustache 설정 / 기본 login 설정

---
+++ loginserver/src/test/java/com/loginServer/loginserver/LoginserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loginServer.loginserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
