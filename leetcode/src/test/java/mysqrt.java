7cb5742e680adcf7f9fa728e1d48889acf4eaf04
Chaeguevara <dfso2@naver.com>
2021-12-11 22:12:10 +0900

    :rotating_light: Sqaure Root fail
     자료구조... Long을 하지 않아서 out of range로 이상한 결과값 나옴. 예제
    해답으로 대체

---
+++ leetcode/src/test/java/mysqrt.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class mysqrt {
+
+    @Test
+    public void testBinary(){
+//        Assertions.assertEquals(2,mySqrt.soultion(5));
+//        Assertions.assertEquals(2,mySqrt.soultion(8));
+//        Assertions.assertEquals(3,mySqrt.soultion(14));
+//        Assertions.assertEquals(4,mySqrt.soultion(16));
+//        Assertions.assertEquals(32,mySqrt.soultion(1024));
+        Assertions.assertEquals(46339,mySqrt.soultion(2147395599));
+
+    }
+}
