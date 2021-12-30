ca04354a0c4b7a56aa92c3f219c409e85b8bb7f6
Chaeguevara <dfso2@naver.com>
2021-12-30 14:48:42 +0900

    FloodFill Fail

--- leetcode/src/main/java/Easy/FloodFill.java
+++ leetcode/src/main/java/Easy/FloodFill.java
@@ -1,19 +1,21 @@
 package Easy;
 
+import java.awt.*;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class FloodFill {
-    public class Coordinate{
+    public class Point{
         public int x;
         public int y;
-
-        Coordinate(int x, int y){
+        Point(int x, int y){
             this.x = x;
             this.y = y;
         }
-
     }
+
     public int[][] solution(int[][] image, int sr, int sc, int newColor) {
         int m = image.length;
         int n = image[0].length;
@@ -21,21 +23,39 @@ public class FloodFill {
             image[0][0]=newColor;
             return image;
         }
-        Set<Coordinate> mVisitied = new HashSet<>();
-        mVisitied.add(new Coordinate(sr,sc));
-        image = floodFillHelper(image,sr,sc,newColor,mVisitied,image[sr][sc],m,n);
+        Map<Point,Integer> visited = new HashMap();
+
+        image = floodFillHelper(image,sr,sc,newColor,image[sr][sc],m,n,visited);
         return image;
     }
 
-    public static int[][] floodFillHelper(int[][] image, int sr, int sc, int newColor,
-                                          Set<Coordinate> visitied, int oldColor,int m, int n){
-        if(sr<0 || sr>m-1){
+    public int[][] floodFillHelper(int[][] image, int sr, int sc, int newColor,
+                                   int oldColor, int m, int n, Map<Point, Integer> visited){
+        if(sr<0 || sr>m-1){ // row out of range
             return image;
         }
-        if(sc<0 || sc>n-1){
+        if(sc<0 || sc>n-1){ // column out of range
             return image;
         }
-        return null;
+        try {
+
+            visited.get(new Point(sc,sr));
+            return image;
+        }catch (NullPointerException e){
+            visited.put(new Point(sc,sr),1);
+        }
+
+        if(image[sr][sc] != oldColor){
+            return image;
+        }
+
+        image[sr][sc] = newColor;
+        image = floodFillHelper(image,sr+1,sc,newColor,oldColor,m,n,visited);
+        image = floodFillHelper(image,sr-1,sc,newColor,oldColor,m,n,visited);
+        image = floodFillHelper(image,sr,sc+1,newColor,oldColor,m,n,visited);
+        image = floodFillHelper(image,sr,sc-1,newColor,oldColor,m,n,visited);
+
+        return image;
 
 
     }
